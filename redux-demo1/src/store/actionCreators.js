03341dd02225a78b2b57031539918441fcf861fd
wujiaqiang <wujiaqiang@icourt.cc>
2019-03-29 15:51:12 +0800

    action的创建封装到actionCreators中

---
+++ redux-demo1/src/store/actionCreators.js
@@ -0,0 +1,6 @@
+import {CHANGE_INPUT_VALUE ,ADD_TODO_ITEM , DELETE_TODO_ITEM } from  './actionTypes'
+
+ export const getInputChangeAction = (value) => {
+     type : CHANGE_INPUT_VALUE , 
+     value 
+ }
--- redux-demo1/src/store/reducer.js
+++ redux-demo1/src/store/reducer.js
@@ -1,3 +1,4 @@
+import {CHANGE_INPUT_VALUE ,ADD_TODO_ITEM , DELETE_TODO_ITEM } from  '. /actionTypes'
 
 const defaultState = {
     inputValue:"123",
@@ -6,21 +7,29 @@ const defaultState = {
 
 export default (state = defaultState,action) => {  // state 指的是 笔记本里的空数据
     console.log(state,action)
-    if(action.type == 'change_input_value'){
+    if(action.type === CHANGE_INPUT_VALUE){
         const newState = JSON.parse(JSON.stringify(state)) // 把传递过来的值复制一份 
         newState.inputValue = action.value
 
         return newState
     }
 
-    if(action.type='add_todo_item'){
+    if(action.type===ADD_TODO_ITEM){
         const newState = JSON.parse(JSON.stringify(state)) // 把传递过来的值复制一份 
-
+        console.log('add_todo_item 前',newState)
         newState.list.push(newState.inputValue)
 
         newState.inputValue = ''
-        console.log(newState)
+        console.log('add_todo_item 后',newState)
+        return newState
     }
+
+    if(action.type === DELETE_TODO_ITEM){
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.index,1);
+        return newState
+    }
+
 
     return state;
 }
